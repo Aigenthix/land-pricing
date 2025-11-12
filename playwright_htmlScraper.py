4b38118942f080d41b13ed61056aca8e53491f75
RudraSomaiya <rudrasomaiya0@gmail.com>
2025-11-12 18:27:37 +0530

    tried adding geofencing to india for playwright

--- playwright_htmlScraper.py
+++ playwright_htmlScraper.py
@@ -2,9 +2,17 @@ from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
     # Launch Chromium (set headless=False if you want to see the browser)
-    browser = p.chromium.launch(headless=false)
-    page = browser.new_page()
-
+    browser = p.chromium.launch(
+        proxy={"server": "http://host:port", "username": "USER", "password": "PASS"},
+        args=["--no-sandbox","--disable-dev-shm-usage"],
+        headless=True
+    )
+    context = browser.new_context(
+        user_agent="Mozilla/5.0 ... Chrome/120 Safari/537.36",
+        locale="en-IN",
+        extra_http_headers={"Accept-Language": "en-IN,en;q=0.9"}
+    )
+    page = context.new_page()
     # Navigate to the URL
     page.goto("https://igreval.maharashtra.gov.in/eASR2.0/eASRCommon.aspx?hDistName=Palghar")
 
